88054098f408f8b0eab9283ce8103f20eb5e0780
harris-jacob <jacob.harris443@gmail.com>
2020-12-09 10:51:35 +0000

    part two

--- src/solutions/day_8.hpp
+++ src/solutions/day_8.hpp
@@ -1,10 +1,11 @@
 #include <istream>
 #include <string>
 #include <vector>
+#include <map>
+#include <set>
 #include <stdexcept>
 #include "read_file.hpp"
 #include "solutions.hpp"
-#include "map"
 
 namespace day8 {
 
@@ -14,7 +15,27 @@ class Day8 : public solutions::Solution {
         return std::make_unique<Day8>();
     }
 
-    std::string partOne(std::istream& input) override {
+    int evaluateOperation(int i, std::pair<std::string, std::string> command) {
+
+        if(command.first == "nop") {
+            i++;
+        }
+        else if(command.first == "acc") {
+            accu += std::stoi(command.second);
+            i++;
+        }
+            
+        else if(command.first == "jmp") {
+            i+=std::stoi(command.second);
+        }
+        else {
+            throw std::runtime_error("unexpected op code :(");
+        }
+
+        return i;
+    }
+
+    std::vector<std::pair<std::string, std::string>> parse(std::istream& input) {
         auto str_input = utils::readStrInput(input);
         std::vector<std::pair<std::string, std::string>> commands;
 
@@ -26,12 +47,16 @@ class Day8 : public solutions::Solution {
             commands.push_back(command);
         }
 
-        int accu = 0;
+        return commands;
+
+    }
+
+    std::string partOne(std::istream& input) override {
+        accu = 0;
         int i=0;
         std::map<int, int> seen;
-
-        while(i<commands.size()) {
-            
+        auto commands = parse(input);
+        while(i < commands.size()) {
             if(seen.count(i) == 0) {
                 seen[i] = 1;
             } else {
@@ -41,29 +66,57 @@ class Day8 : public solutions::Solution {
             if(seen[i] > 1) {
                 return std::to_string(accu);
             }
-
-            if(commands[i].first == "nop") {
-                i++;
-            }
-            else if(commands[i].first == "acc") {
-                accu += std::stoi(commands[i].second);
-                i++;
-            }
-                
-            else if(commands[i].first == "jmp") {
-                i+=std::stoi(commands[i].second);
-            }
-            else {
-                throw std::runtime_error("unexpected op code :(");
-            }
+            i = evaluateOperation(i, commands[i]);
         }
-
-
     }
 
     std::string partTwo(std::istream& input) override {
-        return "";
+        auto commands = parse(input);
+        int i=0;
+        while(i<commands.size()) {
+            if(commands[i].first == "jmp") {
+                commands[i].first = "nop";
+                auto ans = eval(commands);
+                if(ans == -1) {
+                    commands[i].first = "jmp";
+                } else {
+                    return std::to_string(ans);
+                }
+            } else if(commands[i].first == "nop") {
+                commands[i].first = "jmp";
+                auto ans = eval(commands);
+                if(ans == -1) {
+                    commands[i].first = "nop";
+                } else {
+                    return std::to_string(ans);
+                }
+            }
+            i++;
+        }
+        return "-1";
     }
+
+    int eval(std::vector<std::pair<std::string, std::string>>& commands){
+        accu=0;
+        int i=0;
+        std::map<int, int> seen;
+        while(i < commands.size()) {
+            if(seen.count(i) == 0) {
+                seen[i] = 1;
+            } else {
+                seen[i] = 2;
+            }
+
+            if(seen[i] > 1) {
+                return -1;
+            }
+            i = evaluateOperation(i, commands[i]);
+        }
+        return accu;
+    }
+
+  private:
+    int accu = 0;
 
 };
 
