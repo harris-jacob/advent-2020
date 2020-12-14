b1a7b95a17c6c67111aa3a8b5fd4ddc063acc997
harrisja <jacob.harris443@gmail.com>
2020-12-14 18:12:42 +0000

    day 13 part 2

--- test/day13_test.cpp
+++ test/day13_test.cpp
@@ -1,18 +1,28 @@
 #include <istream>
 
-#include "gtest/gtest.h"
 #include "day_13.hpp"
+#include "gtest/gtest.h"
 
 TEST(day_13, partOne) {
 
-      // Arrange
-      std::istringstream input(
-          "939\n"
-          "7,13,x,x,59,x,31,19\n"
-  );
+  // Arrange
+  std::istringstream input("939\n"
+                           "7,13,x,x,59,x,31,19\n");
 
-    auto solution = day13::Day13();
+  auto solution = day13::Day13();
 
-    // Assert
-    EXPECT_EQ(solution.partOne(input), "295");
+  // Assert
+  EXPECT_EQ(solution.partOne(input), "295");
+}
+
+TEST(day_13, partTwo) {
+
+  // Arrange
+  std::istringstream input("939\n"
+                           "1789,37,47,1889\n");
+
+  auto solution = day13::Day13();
+
+  // Assert
+  EXPECT_EQ(solution.partTwo(input), "1202161486");
 }
